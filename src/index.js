9563e0d3854c523f2cda6aea6aa6c338e370f42e
skibyks <skiby_1@mail.ru>
2021-03-14 13:59:49 +0300

    feat: complited

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(minarr){
+  if ( minarr === undefined || minarr.length === 0 ){
+     return 0  
+  } else {
+     return Math.min(...minarr)
+  }
+ 
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(maxarr){
+  if(maxarr === undefined || maxarr.length === 0){
+      return 0
+  } else {
+      return Math.max(...maxarr)
+  }
+  
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(avgarr){
+  if(avgarr === undefined || avgarr.length === 0){
+      return 0
+  } else {
+     return avgarr.length/2 
+  }
+  
+};
